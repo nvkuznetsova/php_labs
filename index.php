d9db15afffeb913a03235dc53efe7a3e402b39b8
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-03-16 05:38:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+  if (isset($_GET['print'])) {
+    header('Content-type: text/plain; charset=utf-8');
+    echo file_get_contents(basename(__FILE__));
+  }
+
+  if (isset($_GET['author'])) {
+    header('Content-type: text/html; charset=utf-8');
+    echo '<h4>Ilya Goss</h4>';
+  }
+  if (isset($_GET['info'])) {
+    phpinfo();
+  }
