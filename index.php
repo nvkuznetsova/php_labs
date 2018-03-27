520ffd426e30b191c84eb3480eba3cd0a1a2d837
nvkuznetsova <nvkuznetsova@list.ru>
2018-03-27 22:32:36 +0300

    modify

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
 
-$date = new class {
-
-  function __construct($today = date('d/m/Y H:i'))
-  {
-    # code...
-    $this->today = $today;
-  }
-  function getDate() {return $this->today;}
-};
-
-  if (isset($_GET)) {
-    # code...
-    echo($date->getDate());
-  }
-
   if (isset($_GET['print'])) {
     header('Content-type: text/plain; charset=utf-8');
     echo file_get_contents(basename(__FILE__));
@@ -27,8 +12,17 @@ $date = new class {
   if (isset($_GET['info'])) {
     phpinfo();
   }
+
   if (isset($_GET['public'])) {
     header('Access-Constrol-Allow-Origin: *');
     header('Content-type: text/plain; charset=utf-8');
     header('Access-Control-Allow-Methods: GET,POST,DELETE');
+  }
+
+  $date = new class {
+      function getDate() {return date('d/m/Y H:i');}
+  };
+
+  if (isset($_GET)) {
+    echo($date->getDate());
   }
