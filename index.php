293fe3f52c24cf6fbb16aef8752bba7ddf0b88b1
nvkuznetsova <nvkuznetsova@list.ru>
2018-03-27 22:24:42 +0300

    add class

--- index.php
+++ index.php
@@ -7,8 +7,25 @@
 
   if (isset($_GET['author'])) {
     header('Content-type: text/html; charset=utf-8');
-    echo '<h4>Кузнецова Наталья</h4>';
+    echo '<h4 id="author" title="GossJS">Кузнецова Наталья</h4>';
   }
   if (isset($_GET['info'])) {
     phpinfo();
   }
+  if (isset($_GET['public'])) {
+    header('Access-Constrol-Allow-Origin: *');
+    header('Content-type: text/plain; charset=utf-8');
+    header('Access-Control-Allow-Methods: GET,POST,DELETE');
+  }
+
+  $date = new class {
+
+    function __construct($today = date('d/m/Y H:i'))
+    {
+      # code...
+      $this->today = $today;
+    }
+    function getDate() {return $this->today;}
+  };
+
+  echo($date->getDate());
