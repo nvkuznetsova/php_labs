8c811662317371c73068c44a824d93f0cc036f57
nvkuznetsova <nvkuznetsova@list.ru>
2018-04-01 21:04:59 +0300

    index

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
 
   $app = new Silex\Application();
 
+  $app->get('/', function() {
+    return '<h1>Hello</h1>';
+  });
+
   $app->get('/hello/{name}', function ($name) use ($app) {
       return 'Hello '.$app->escape($name);
   });
 
   $app->get('/date', function(){
+      date_default_timezone_set('Europe/Kiev');
       return date('r');
   });
 
