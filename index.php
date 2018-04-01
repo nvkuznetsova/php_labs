64bcab583531a36135ae9f3c3a88153796fa28ae
nvkuznetsova <nvkuznetsova@list.ru>
2018-04-01 21:16:47 +0300

    update index

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
   $app = new Silex\Application();
 
   $app->get('/', function() {
-    return '<h1>Задания</h1><a href="/date">Тут дата</a>
-    <a href="/hello/world!">Тут приветствие</a>';
+    return '<h1>Задания</h1>
+    <a href="/dateClass.php">Задание1. Класс дата</a>
+    <a href="/date">Тут дата</a>
+    <a href="/hello/world!">Тут приветствие</a>
+    <a href="/print">Печать текста</a>
+    <a href="/author">Автор</a>';
   });
 
   $app->get('/hello/{name}', function ($name) use ($app) {
@@ -15,7 +19,21 @@
 
   $app->get('/date', function(){
       date_default_timezone_set('Europe/Kiev');
-      return date('r');
+      return '<h3>'.date('r').'</h3>';
+  });
+
+  $app->get('/print', function() {
+    header('Content-type: text/plain; charset=utf-8');
+    return file_get_contents(basename(__FILE__));
+  });
+
+  $app->get('/author', function() {
+    header('Content-type: text/html; charset=utf-8');
+    return '<h4 id="author" title="GossJS">Кузнецова Наталья</h4>';
+  });
+
+  $app->get('/info', function() {
+    return phpinfo();
   });
 
 	$app->run();
