05e2d4e06be1eb7f17f5d2fabf98cf660759afac
nvkuznetsova <nvkuznetsova@list.ru>
2018-04-01 20:59:15 +0300

    index

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php
+< ?php
+
   require 'vendor/autoload.php';
 
   $app = new Silex\Application();
+
   $app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+      return 'Hello '.$app->escape($name);
   });
 
   $app->get('/date', function(){
-    date_default_timezone_set('Europe/Kiev');
-    return date('d/m/Y H:i');
+      return date('r');
   });
-
-  $app->run();
- ?>
+  
+	$app->run();
