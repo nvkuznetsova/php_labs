111daf859033f83c7aed81593a36c8bc5ba2f030
nvkuznetsova <nvkuznetsova@list.ru>
2018-04-12 21:54:17 +0300

    font

--- hist.php
+++ hist.php
@@ -27,7 +27,7 @@ $textcolor = imagecolorallocate($ctx, 0, 0, 0);
 
 imagefilledrectangle($ctx,$x1,$y1,$x2,$y2,$red);
 //imagestring($ctx, 2, $x1+5, 280, $names[$i], $textcolor);
-imagettftext($ctx, 10, 0, $x1+$pad, 280, $textcolor, "DejaVuSans.ttf", $names[$i]); 
+imagettftext($ctx, 10, 0, $x2-$x1, 285, $textcolor, "DejaVuSans.ttf", $names[$i]); 
 $i++;
 });
 
